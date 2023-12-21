9283445017c3182d5003bd3df722093c078b2367
maxbirman <63321881+maxbirman@users.noreply.github.com>
2023-12-21 15:38:13 -0300

    fg

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/;
+var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
 
 //asigna funcion al clickear en "siguiente" - oculta div actual y pasa al siguiente
