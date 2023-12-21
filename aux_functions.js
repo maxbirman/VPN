514a7384d27ab8d119be9b8dacfd2cc40d075326
maxbirman <63321881+maxbirman@users.noreply.github.com>
2023-12-21 18:19:20 -0300

    fefe

--- aux_functions.js
+++ aux_functions.js
@@ -63,3 +63,45 @@ function ipCorrecta (ip) {
 function emailCorrecto(email) {
     return emailRegex.test(email);    
 }
+
+function getOptionsFromArray (data, selected){
+    var options = "";
+    var filas = data.split("\n");
+
+    for (var i = 0; i < filas.length; i ++) {
+        var listaInterface = filas[i].split(";");
+        
+        if(listaInterface[0] == selected){
+            //se saltea 0 y 1 porque corresponden al modelo del Equipo
+            for (var j = 2; j < listaInterface.length; j ++){
+                if(j== (listaInterface.length) - 1) {
+                    options += listaInterface[j] + ";" + listaInterface[j];
+                }else {
+                    options += listaInterface[j] + ";" + listaInterface[j] + "\n";
+                }
+            }                            
+        }                       
+    }
+    return options;
+}
+
+//funcion para popular cualquier select
+function populateSelect (select, data) {
+    var options = data.split('\n');
+    for (var i = 0; i < options.length; i++){
+        var option = options[i].split(";");
+        select.append('<option value="' + option[0] + '">' + option[1] + '</option>');
+    }
+}
+
+//toma el contenido de un archivo de texto y devuelve un array
+function getArrayFromFile(file, callback) {
+    $.ajax({
+        url: file,
+        datatype: "text",
+        success: function (data) {
+            data.split('\n');
+            callback(data);
+        }
+    });
+}
