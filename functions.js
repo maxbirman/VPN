022797b8168089d71d5da66c8a9a3b2df0a81dc4
maxbirman <63321881+maxbirman@users.noreply.github.com>
2023-12-28 09:40:31 -0300

    dsdsd

--- functions.js
+++ functions.js
@@ -110,7 +110,7 @@ function populateInterfaces(){
     var select = $("#deviceModel");
     var selected = select.val();
     select = $("#interface");
-    var file = "/interfaces.txt";
+    var file = "https://raw.githubusercontent.com/maxbirman/VPN/main/interfaces.csv";
     var data = [];
      
     getArrayFromFile(file, function(extData) {
