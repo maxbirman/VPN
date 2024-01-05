0a12785112042c7981aef3b29fc60c831317fee7
maxbirman <63321881+maxbirman@users.noreply.github.com>
2024-01-05 08:29:51 -0300

    sdsdsd

--- aux_functions.js
+++ aux_functions.js
@@ -86,7 +86,7 @@ function verificarCamposCompletos(divId, siguiente, input) {
                             $("#remoteMask_0").attr('disabled', 'disabled');
                         } else{
                             $("#localMask_0").empty();
-                            $("#localMask_0").append(`<option value="none" selected="selected" disabled>--</option>`);
+                            $("#localMask_0").append('<option value="none" selected="selected" disabled>--</option>');
                             populateMask($("#localMask_0"));
                             $("#localMask_0").removeAttr('disabled');          
                             errorSubnet.attr('style', 'color: red; padding-left: 2px; display: none');     
