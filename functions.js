dda79d27464726a01c1f615254359a21bc7186c0
maxbirman <63321881+maxbirman@users.noreply.github.com>
2024-01-12 15:28:54 -0300

    dfdfdf

--- functions.js
+++ functions.js
@@ -342,7 +342,7 @@ function addPolicy(index){
                     <div class="col-1">
                         <div class="row">
                             <div class="col-6" id="removePolicyButton_${index}">
-                                <button type="button" class="btn btn-primary" tabindex="-1" role="button" aria-disabled="true" id="addPolicy_${index}" onclick="addPolicy(${nextIndex})" disabled>-</a>
+                                <button type="button" class="btn btn-primary" tabindex="-1" role="button" aria-disabled="true" id="RemovePolicy_${index}" onclick="RemovePolicy(${nextIndex})" disabled>-</a>
                             </div>
                             <div class="col-6" id="addPolicyButton_${index}">
                                 <button type="button" class="btn btn-primary" tabindex="-1" role="button" aria-disabled="true" id="addPolicy_${index}" onclick="addPolicy(${nextIndex})">+</a>
